d5d43e8decb4254352d6f071e7fd2dfa6008b95e
Firas Abdelaziz <firasabdelaziz123@gmail.com>
2024-07-25 12:43:33 +0100

    fix : change name

--- Customer/src/main/java/com/example/customer/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customer.repositories;
-import com.example.customer.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends MongoRepository<Customer,String> {
-}
